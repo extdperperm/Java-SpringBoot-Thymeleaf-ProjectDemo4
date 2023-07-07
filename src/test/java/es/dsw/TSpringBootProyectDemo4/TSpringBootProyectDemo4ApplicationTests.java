96dff165f692e286420ec72359a31ee93a2aa370
Dabiel Pérez <ext.dperperm@gmail.com>
2023-07-07 23:29:33 +0100

    Proyecto ejemplo JDBC

---
+++ src/test/java/es/dsw/TSpringBootProyectDemo4/TSpringBootProyectDemo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dsw.TSpringBootProyectDemo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TSpringBootProyectDemo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
